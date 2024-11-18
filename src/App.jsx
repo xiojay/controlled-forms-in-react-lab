5e74628408c047b7032af6a6201a7b7301251716
xiojay <jocey.ale24@gmail.com>
2024-11-18 17:58:57 -0500

    Mybookshelf

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import './index.css';
+import './components/Bookshelf.css'
+import Bookshelf from './components/Bookshelf.jsx';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
+
